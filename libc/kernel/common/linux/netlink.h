297613c124675139340ccf85abc596777b1a34a6
stelios97 <stilianos1000@gmail.com>
2012-12-09 14:47:03 +0200

    fix wifi build

--- libc/kernel/common/linux/netlink.h
+++ libc/kernel/common/linux/netlink.h
@@ -18,8 +18,8 @@
  ****************************************************************************/
 #ifndef __LINUX_NETLINK_H
 #define __LINUX_NETLINK_H
-#include <linux/socket.h>  
 #include <linux/types.h>
+#include <linux/socket.h>  
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define NETLINK_ROUTE 0  
 #define NETLINK_UNUSED 1  
